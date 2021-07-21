a9052faebe604dcb6d103e285022525a7fb98069
Quentin MOLERO <quentin.molero@estiam.com>
2021-07-21 13:01:28 +0200

    Refactored directive for name color to type color in the logs

--- src/app/components/battle/battle.component.ts
+++ src/app/components/battle/battle.component.ts
@@ -15,7 +15,7 @@ export class BattleComponent implements OnInit {
     name: 'carapuce',
     health: 80,
     speed: 80,
-    type: 'blue',
+    type: 'water',
     attacks: [
       {
         name: 'charge',
@@ -41,7 +41,7 @@ export class BattleComponent implements OnInit {
     name: 'pikachu',
     health: 80,
     speed: 70,
-    type: 'yellow',
+    type: 'electric',
     attacks: [
       {
         name: 'charge',
