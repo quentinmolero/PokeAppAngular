62280be10eb5a1bc709e4faed7100ce1cba47980
RemyMach <remygt@hotmail.fr>
2021-07-23 15:08:03 +0200

    fix test list pokemon service

--- src/app/services/poke-api/list-pokemon.service.spec.ts
+++ src/app/services/poke-api/list-pokemon.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ListPokemonService } from './list-pokemon.service';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('ListPokemonService', () => {
   let service: ListPokemonService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ListPokemonService]
+    });
     service = TestBed.inject(ListPokemonService);
   });
 
