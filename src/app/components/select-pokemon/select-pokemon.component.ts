5076d83a3eecf87e764212600b07232095f5efe6
RemyMach <remygt@hotmail.fr>
2021-07-28 11:49:45 +0200

    add end of select-pokemon component

--- src/app/components/select-pokemon/select-pokemon.component.ts
+++ src/app/components/select-pokemon/select-pokemon.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-select-pokemon',
@@ -6,10 +7,26 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./select-pokemon.component.css']
 })
 export class SelectPokemonComponent implements OnInit {
+  pokemonChoose1: string | undefined;
+  pokemonChoose2: string | undefined;
+  error: string | undefined;
 
-  constructor() {
+  constructor(
+    private router: Router
+  ) {
   }
 
   ngOnInit(): void {
+  }
+  getPokemonChoose1(pokemon: string): void {
+   this.pokemonChoose1 = pokemon;
+  }
+  getPokemonChoose2(pokemon: string): void {
+    this.pokemonChoose2 = pokemon;
+  }
+  validateChoicePokemon(): void {
+    if (this.pokemonChoose1 && this.pokemonChoose2) {
+      this.router.navigate([`/battle/${this.pokemonChoose1}/${this.pokemonChoose2}`]);
+    }
   }
 }
